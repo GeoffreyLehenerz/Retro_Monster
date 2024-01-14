ea74bbfb3e3cfd966f5dbe08ec07335b85b991cc
Geoffrey Lehenerz <glehenerz@gmail.com>
2024-01-14 14:27:31 +0100

    découpage Template et gestion images

---
+++ resources/views/templates/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+@include('templates.partials._head')
+    <body class="bg-gray-800 text-white font-sans">
+        <!-- Header -->
+        @include('templates.partials._header')
+
+        <!-- Main Content -->
+        <div class="container mx-auto flex flex-wrap pt-4 pb-12">
+
+        @include('templates.partials._main')
+
+        <!-- Sidebar -->
+        @include('templates.partials._aside')
+        </div>
+
+        <!-- Footer -->
+        @include('templates.partials._footer')
+    </body>
+</html>
