067bf73444b2d588a529656c7ea0ba4a69e5fac9
sphins <glehenerz@gmail.com>
2024-01-15 08:43:49 +0100

    contrainte generale des route id et slug

--- documents/db/db_remplie_sphins.sql
+++ documents/db/db_remplie_sphins.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `retro_monsters` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `retro_monsters`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: retro_monsters
